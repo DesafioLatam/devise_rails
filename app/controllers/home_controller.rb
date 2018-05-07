f95f99e0d99dd400b9dc751b841b9ef5f3495c67
erwin aguero <erwin@agiledevers.com>
2018-05-07 14:58:52 -0300

    Readme about objectives for branche 02

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  #Source: https://github.com/nsarno/knock/issues/138
+  skip_before_action :authenticate_user, raise: false
+
   def index
+    
   end
 end
